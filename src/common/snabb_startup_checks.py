d12e1216e80b8a220ac478899c2bf57c32f93711
Amish Anand <aamish@juniper.net>
2017-01-31 11:21:21 -0800

    COnfiguration parsing and callbacks added along with the templates

---
+++ src/common/snabb_startup_checks.py
@@ -0,0 +1,14 @@
+__author__ = "Amish Anand"
+__copyright__ = "Copyright (c) 2017 Juniper Networks, Inc."
+
+"""
+We need to check whether there are existing config files available for Snabb or not
+If they are present, then we need to start the snabb instances based on the configuration
+"""
+
+def snabbStartupConfig():
+    # Check if the files are present or not.
+    # Set the parameters and start the snabb application
+    pass
+
+
--- src/conf/callback.py
+++ src/conf/callback.py
@@ -4,6 +4,9 @@ __copyright__ = "Copyright (c) 2015 Juniper Networks, Inc."
 from common.mylogging import LOG
 from conf_globals import dispQ as dispQ
 import os
+import op.protos.openconfig_service_pb2 as openconfig_service_pb2
+import common.app_globals
+import json
 
 class SnabbCallback:
     def __init__(self, dev):
@@ -13,45 +16,37 @@ class SnabbCallback:
         global dispQ
         LOG.info("Inside Handle commit notifications")
         LOG.info("Received message: %s" %str(message))
-        # config_dict = message['commit-patch']['configuration']
-        # try:
-        #     sw_present = False
-        #     for keys in config_dict:
-        #         if keys.endswith('softwire-config'):
-        #             sw_present = True
-        #             break
-        #     if sw_present:
-        #         # Push all the configuration into a new file
-        #         try:
-        #             pyez_client = Device(host=self._dev._host, user=self._dev._auth_user, password=self._dev._auth_pwd, gather_facts=False)
-        #             pyez_client.open()
-        #         except Exception as e:
-        #             LOG.critical('Failed to connect to the device, exception: %s' %e.message)
-        #             return
-        #         try:
-        #             json_query = """
-        #                          <configuration>
-        #                              <softwire-config>
-        #                             </softwire-config>
-        #                         </configuration>
-        #                         """
-        #             result = pyez_client.rpc.get_config(filter_xml=etree.XML(json_query), options={'format': 'json'})
-        #             pyez_client.close()
-        #             LOG.info('Applied the pyez query, got this result: %s' %result)
-        #             config_dict = result["configuration"]["softwire-config"]
-        #             LOG.debug("Notification message contains the config %s" %(str(config_dict)))
-        #             dispQ.put(config_dict)
-        #         except Exception as e:
-        #             LOG.info("The softwire-config was deleted by the user, purging all the config files")
-        #             config_dict = {'purge': 1}
-        #             dispQ.put(config_dict)
-        #         return
-        #     else:
-        #         LOG.info("Softwire-config not present in the notification")
-        #
-        # except Exception as e:
-        #     LOG.critical("Exception: %s" %e.message)
-        #     LOG.info('Exiting the JET app')
-        #     os._exit(1)
+        config_dict = message['commit-patch']
+        print config_dict
+        try:
+            sw_present = False
+            for keys in config_dict:
+                if keys.endswith('softwire-config'):
+                    sw_present = True
+                    break
+            if sw_present:
+                # Push all the configuration into a new file
+                stub = openconfig_service_pb2.beta_create_OpenconfigRpcApi_stub(self._dev.getChannel())
+
+                get_request = openconfig_service_pb2.GetRequestList(operation_id="1001", operation=1,
+                                                                   path="/configuration/ietf-softwire:softwire-config")
+                request = openconfig_service_pb2.GetRequest(request_id=1002, encoding=1, get_request=[get_request])
+                response = stub.Get(request, common.app_globals.RPC_TIMEOUT_SECONDS)
+                for rsp in response.response:
+                    print rsp.value
+                    if rsp.response_code == openconfig_service_pb2.OK and rsp.value != "":
+                        print rsp
+
+                        LOG.info(
+                            "Invoked the getRequest for snabb configuration")
+                        config_dict = json.loads(rsp.value)["ietf-softwire:softwire-config"]
+                        LOG.debug("Notification message contains the config %s" %(str(config_dict)))
+                        dispQ.put(config_dict)
+            else:
+                LOG.info("Softwire-config not present in the notification")
+        except Exception as e:
+            LOG.critical("Exception: %s" %e.message)
+            LOG.info('Exiting the JET app')
+            os._exit(1)
         return
 
