0f79f5e00c2c75683e42786047b4364caecaecee
Marcel Wiget <mwiget@juniper.net>
2017-07-21 13:56:10 +0200

    enable lldp on all ports for packetblaster to learn the dst_mac to use

---
+++ gen-pci-traffic.sh
@@ -0,0 +1,3 @@
+# [wip]: must wrap this in a container
+
+sudo src/snabb packetblaster lwaftr --src_mac 02:02:02:02:02:02 --b4 2001:db8::40,10.10.0.0,1024 --aftr 2001:db8:ffff::100 --count 60000 --rate .5 --pci 04:00.1
